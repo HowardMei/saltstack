c876c8ebaf7a5953c670fb68d12e9d5bb1e7a9df
HowardMei <howardleomei@gmail.com>
2015-06-12 16:20:03 +0800

    develop: replace all saltstack bootstrap-salt.sh with github mubiic/saltstrap/master/bootstrap-salt.sh

--- salt/cloud/deploy/curl-bootstrap.sh
+++ salt/cloud/deploy/curl-bootstrap.sh
@@ -4,10 +4,10 @@
 # This is a generic wrapper for the salt-bootstrap script at:
 #
 # https://github.com/saltstack/salt-bootstrap
-# 
+#
 # It has been designed as an example, to be customized for your own needs.
 
-curl -L https://bootstrap.saltstack.com | sudo sh -s -- "$@"
+curl -L https://raw.githubusercontent.com/mubiic/saltstrap/master/bootstrap-salt.sh | sudo sh -s -- "$@"
 
 # By default, Salt Cloud now places the minion's keys and configuration in
 # /tmp/.saltcloud/ before executing the deploy script. After it has executed,
