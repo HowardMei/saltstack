da618de20ba34f15fdb59bb6b22e9205b6e81102
HowardMei <howardleomei@gmail.com>
2015-06-12 17:17:15 +0800

    develop: add skip_verify option to file.managed states module with a simple presetting trick

--- salt/states/file.py
+++ salt/states/file.py
@@ -1019,6 +1019,7 @@ def missing(name):
 def managed(name,
             source=None,
             source_hash='',
+            skip_verify=False,
             user=None,
             group=None,
             mode=None,
@@ -1150,6 +1151,17 @@ def managed(name,
                     - source: https://launchpad.net/tomdroid/beta/0.7.3/+download/tomdroid-src-0.7.3.tar.gz
                     - source_hash: https://launchpad.net/tomdroid/beta/0.7.3/+download/tomdroid-src-0.7.3.tar.gz/+md5
 
+    skip_verify
+        Skip the source hash/checksum verification step to avoid warning
+            example:
+
+            .. code-block:: yaml
+
+                tomdroid-src-0.7.3.tar.gz:
+                  file.managed:
+                    - name: /tmp/tomdroid-src-0.7.3.tar.gz
+                    - source: https://launchpad.net/tomdroid/beta/0.7.3/+download/tomdroid-src-0.7.3.tar.gz
+                    - skip_verify: True
 
     user
         The user to own the file, this defaults to the user salt is running as
@@ -1457,6 +1469,7 @@ def managed(name,
                 ret,
                 source,
                 source_sum,
+                skip_verify,
                 user,
                 group,
                 mode,
@@ -1509,6 +1522,7 @@ def managed(name,
                 ret,
                 source,
                 source_sum,
+                skip_verify,
                 user,
                 group,
                 mode,
